f45b2bce39ca80aff24d0baf63edb9e7a60c1ae4
Ales919 <holi9ale@gmail.com>
2016-07-18 00:49:00 +0300

    Add main.cpp,google's tutorial codes

---
+++ main.cpp
@@ -0,0 +1,179 @@
+//
+//  main.cpp
+//  googleTutorial
+//
+//  Created by Osana Alexandros on 12/7/16.
+//  Copyright © 2016 AlexOsna. All rights reserved.
+//
+
+
+#include <iostream>
+#include <fstream>
+//#include <string>
+using namespace std;
+
+
+
+/*
+float salary1 (int sales)
+{
+    float s1 = 600 * 4;
+    return s1;
+}
+
+float salary2(int sales)
+{
+    float s2 = 40 * 4 * 7 + (0.1 * (sales *225));
+    return s2;
+}
+
+float salary3(int sales)
+{
+    float s3 = (0.1 * (sales *225)) + 20 * sales;
+    return s3;
+}
+*/
+
+float input_grade(char *msg)
+{
+    float value;
+    cout << msg;
+    if (!(cin >>value))
+    {
+        cout << "wrong input";
+        exit(0);
+    }
+    return value;
+}
+
+
+int main() {
+   //=========================================================================
+    /*
+    int input_var = 0;
+    	do {
+        cout << "Enter a number (-1 = quit): ";
+        // The following line accepts input from the keyboard into
+        // variable input_var.
+        // cin returns false if an input operation fails, that is, if
+        // something other than an int (the type of input_var) is entered.
+        if (!(cin >> input_var)) {
+            cout << "You entered a non-numeric. " << endl<< endl;
+            cin.clear();
+            cin.ignore(1000,'\n');
+        }
+        else if (input_var == -1) {
+            cout << "You entered  the exit option: " << input_var << endl << endl;
+        }
+        else{
+             cout << "You entered: " << input_var << endl<< endl;
+        }
+            
+            
+    } while (input_var != -1);
+    cout << "All done." << endl;
+     */
+    //-----------------------------------------------------
+    /*
+    cout << "   1\t2\t3\t4\t5\t6\t7\t8\t9" << endl << "   -\t-\t-\t-\t-\t-\t-\t-\t-\t" << endl;
+    for (int c = 1; c < 10; c++) {
+        cout << c << "| ";
+        for (int i = 1; i < 10; i++) {
+            cout << i * c << '\t';
+        }
+        cout << endl;
+    }
+    */
+    //-----------------------------------------------------
+    /*
+    int guess;
+    
+    srand (time(NULL));
+    int secret_num = rand() % 11;
+    bool terminationFlag = 0;
+    cout<< "Let us play! Guess a number between 0 and 10"<< endl <<"-> ";
+    do{
+    cin >> guess;
+    
+    if ((!(guess)) || (guess >10) || (guess < 0)) {
+        cout << "You entered a wrong element. Guess again from 0-10 " << endl<< endl;
+        cin.clear();
+        cin.ignore(100,'\n');
+    }
+    else{
+        if (guess > secret_num) {
+            cout<< " Guess lower"<<endl;
+        }
+        else if (guess == secret_num){
+            cout<< "You got it...GG !!"<< endl<< endl;
+            terminationFlag = 1;
+        }
+        else{cout<< " Guess higher"<<endl;
+        }
+    }
+    }while (terminationFlag == 0);
+    
+    */
+    
+    //-----------------------------------------------------
+    /*
+    string str1 = "To be or not to be, that is the question";
+    string str2 = "only ";
+    string str3 = str1.substr(6, 12);
+    str1.insert(32, str2);
+    str1.replace(str1.find("to be", 0), 5, "to jump");
+    str1.erase(9, 4);
+    cout << str1 << endl;
+    for (int i = 0; i < str3.length(); i++)
+        cout << str3[i]; cout << endl;
+    */
+    //-----------------------------------------------------
+    //int sales = 100;
+    /*
+    for (int sales=0; sales < 101; sales = sales +15){
+    
+        cout<< "Choice 1: For "<< sales<<" ----> "<< salary1(sales)<<" $ per month"<< endl;
+        cout<< "Choice 2: For "<< sales<<" ----> "<< salary2(sales)<<" $ per month"<< endl;
+        cout<< "Choice 3: For "<< sales<<" ----> "<< salary3(sales)<<" $ per month"<< endl<<endl;
+
+    }
+     */
+    //-----------------------------------------------------
+    /*
+    char first_name[30], last_name[30]; int age;
+    char file_name[20];
+    // Collect the data.
+    cout << "Enter First Name: "; cin >> first_name;
+    cout << "Enter Last Name: "; cin >> last_name;
+    cout << "Enter Age: "; cin >> age;
+    cout << endl << "Enter the name of the file: "; cin >> file_name;
+    
+    // Create an ofstream called People, open the stream for output.
+    ofstream People(file_name, ios::out);
+    // Write the output to the stream.
+    People << first_name << endl << last_name << endl << age << endl;
+    */
+    //-----------------------------------------------------
+    float Final = 0, assignements = 0, midermEx = 0, finalEx = 0, participation = 0;
+    
+    char assign_msg[] = "Assigments grade (1-100): ", midermEx_msg = "Midterm grade (1-100): ";
+    assignements =  input_grade(msg);
+    midermEx = input
+    
+    
+    Final = 0.4*assignements + 0.15*midermEx + 0.35*finalEx + 0.1*participation;
+    
+    
+    //-----------------------------------------------------
+    //=========================================================================
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
